5ed7276ef5f0ac7639416ccde2da7d997ebaecc7
Akhil E <akhil.e2k@gmail.com>
2023-11-14 16:02:17 -0500

    update config

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -1,3 +1,20 @@
+// import { searchPlugin } from "@vuepress/plugin-search";
+// import { defineUserConfig } from "vuepress";
+// import theme from "./theme.js";
+
+// export default defineUserConfig({
+//   lang: "en-US",
+//   title: "MLNLP Blog",
+//   description: "A Blog for Machine Learning, Natural Language Processing, and Data Mining",
+//   base: "/blog/",
+//   theme,
+//   plugins: [
+//     searchPlugin({
+//       // your options
+//     }),
+//   ],
+// });
+
 import { searchPlugin } from "@vuepress/plugin-search";
 import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
@@ -8,6 +25,16 @@ export default defineUserConfig({
   description: "A Blog for Machine Learning, Natural Language Processing, and Data Mining",
   base: "/blog/",
   theme,
+  head: [
+    ['script', {
+      async: true,
+      src: 'https://polyfill.io/v3/polyfill.min.js?features=es6',
+    }],
+    ['script', {
+      async: true,
+      src: 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js',
+    }],
+  ],
   plugins: [
     searchPlugin({
       // your options
